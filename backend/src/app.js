13430ba4f2b2639bcff788f0b17e37d9188bb9dc
marcfyk <marcjfyk@gmail.com>
2019-09-04 22:12:43 +0800

    add comments on code structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,23 @@
+// npm packages
 const express = require('express')
 const bodyParser = require('body-parser')
+
+// local module exports
+const mountRoutes = require('./routes/index')
+
+// port
 const PORT = process.env.PORT || 3000
 
+// init express app
 const app = express()
-
+// set up body parser middleware to parse data
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// mounts all the routes to the app
+mountRoutes(app)
+
+// if all routes not caught -> app sends 404
 app.use((req, res, next) => {
   res.sendStatus(404)
 })
