56e8043a8f814e37355946f3c0e45b8ed06565fd
marcfyk <marcjfyk@gmail.com>
2019-09-07 15:35:35 +0800

    rename routes folder

--- backend/src/routes/account.js
+++ backend/src/routes/account.js
@@ -3,8 +3,13 @@ const express = require('express')
 
 // local module exports
 const db = require('../db')
+const bcrypt = require('../util/bcrypt-util')
 
 // router set up
 const router = express.Router()
+
+router.post("/login", async (req, res) => {
+
+})
 
 module.exports = router
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,8 @@
 // routes
-const auth = require('./auth')
+const account = require('./account')
 const user = require('./user')
 
 module.exports = app => {
-  app.use(auth)
+  app.use(account)
   app.use(user)
 }
