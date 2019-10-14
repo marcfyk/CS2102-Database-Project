cf99520e1913012192d28dfe83f645e391f2ecb1
ryanty <tanyuryan@gmail.com>
2019-10-14 23:38:38 +0800

    refactor login.js and login.ejs

--- website/routes/login.js
+++ website/routes/login.js
@@ -1,42 +1,31 @@
-var express = require('express');
-var router = express.Router();
-
-const { Pool } = require('pg')
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
+const util = require('../util/aux');
+const express = require('express');
+const router = express.Router();
+const pool = util.getPool();
 
 router.get('/', function(req, res, next) {
-  res.render('login', {
-    pageTitle: "Log In",
-    description: "desc-placeholder-login.js"
-  });
+  util.render(res, 'login', 'Login');
 });
 
 router.post('/', function(req, res, next) {
   const user = req.body.username;
   const pass = req.body.password;
-  console.log("entered");
 
   if (!user || !pass) {
-    res.render('login', {
-      pageTitle: "Log In",
-      description: "desc-placeholder-login.js",
-      isEmpty: true
-    });
-
-  } else {
-
-    const query = `SELECT 1 FROM Account WHERE username='${user}' AND password='${pass}'`;
-    pool.query(query, (err, data) => {
-      if (err) {
-        res.redirect('/login');
-      } else {
-        res.redirect('/');
-      }
-    });
-
+    util.render(res, 'login', 'Login', { isEmpty: true });
+    return;
   }
+
+  const query = `SELECT 1 FROM Account
+      WHERE username='${user}' AND password='${pass}'`;
+  pool.query(query, (err, data) => {
+    if (err) {
+      res.redirect('/login');
+    } else {
+      res.redirect('/');
+    }
+  });
+
 });
 
 module.exports = router;
