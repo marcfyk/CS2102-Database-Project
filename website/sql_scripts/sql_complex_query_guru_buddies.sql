fd08f5820b7224b252d898e52d7a8235f4d0d4d0
Stephen Tang <stephentang1217@gmail.com>
2019-11-09 23:03:38 +0800

    aaa

--- website/sql_scripts/sql_complex_query_guru_buddies.sql
+++ website/sql_scripts/sql_complex_query_guru_buddies.sql
@@ -8,11 +8,12 @@ Y AS (
     SELECT X.username
     FROM X
     ORDER BY SUM, AVG
+    LIMIT 10
 )
 SELECT *
 FROM Y y1, Y y2
 WHERE (y1.username, y2.username) IN (
-    SELECT F1.followed, F2.follower
+    SELECT F1.follower, F2.follower
     FROM Follows F1, Follows F2
     WHERE F1.followed = F2.follower
 );
